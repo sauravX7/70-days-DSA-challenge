6a13031dba2ed0bd0bab61614f779ed32be1b1eb
Saurav <130975192+sauravX7@users.noreply.github.com>
2025-07-24 21:40:36 +0530

    Update Question.py

--- Day03/Question.py
+++ Day03/Question.py
@@ -1,1 +1,18 @@
+# LeetCode 3136 Valid Word 
+
+class Solution:
+    def isValid(self, word: str) -> bool:
+        if len(word)< 3:
+            return False 
+        v=False
+        co=False
+        for c in word:
+            if c.isalpha():
+                if c.lower() in 'aeiou':
+                    v=True
+                else:
+                    co=True
+            elif not c.isdigit():
+                return False
+        return v and co
 
