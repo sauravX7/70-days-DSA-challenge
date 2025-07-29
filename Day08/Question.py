015c8b399ca46c2190f3af77c458df0ef682ec97
Saurav <130975192+sauravX7@users.noreply.github.com>
2025-07-29 23:15:52 +0530

    Update Question.py

--- Day08/Question.py
+++ Day08/Question.py
@@ -1,1 +1,24 @@
+# LeetCode 2529. Maximum Count of Positive And Negative Integer
+
+class Solution(object):
+    def maximumCount(self, nums):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] < 0:
+                l = m + 1  
+            else:
+                r = m - 1  
+        neg_count = l 
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] <= 0:
+                l = m + 1
+            else:
+                r = m - 1
+        pos_count = len(nums) - l  
+        return max(neg_count, pos_count)
+
+
 
